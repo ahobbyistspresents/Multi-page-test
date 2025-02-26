502338c74be1aafa8049b07378be86a72f06e625
ahobbyistspresents <ahobbyistspresents@gmail.com>
2025-02-26 17:46:35 +0000

    Update and rename script.js to loadComponent.js

---
+++ loadComponent.js
@@ -0,0 +1,9 @@
+function loadComponent(id, file) {
+    fetch(file)
+        .then(response => response.text())
+        .then(data => document.getElementById(id).innerHTML = data);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    loadComponent("header", "header.html");
+    loadComponent("footer", "footer.html");
